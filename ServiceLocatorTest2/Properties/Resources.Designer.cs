7ecf35661a03839ddae7f0932598c18b7e4fe02a
David Starr <david@elegantcode.com>
2022-11-29 10:30:10 -0800

    renaming

--- ServiceLocatorTest2/Properties/Resources.Designer.cs
+++ ServiceLocatorTest2/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ServiceLocatorIdea.Properties {
+namespace WordReversal.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ServiceLocatorIdea.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ServiceLocatorIdea.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WordReversal.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
